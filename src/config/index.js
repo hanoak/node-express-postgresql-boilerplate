734b59a469df497cf5722b15d298de2bb792f5c3
hanoak <hanoak4online@gmail.com>
2025-05-04 16:11:17 +0530

    chore: Basic folder structure setup.

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+// Add all your app's config here along with envs
+
+const config = {
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+};
+
+module.exports = config;
---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+// Add all your app's constants here.
+
+const constants = {
+  ERROR_TEXTS: {
+    INTERNAL_ERROR: "Internal error, please try again.",
+    BAD_REQ: "Invalid data provided.",
+  },
+  HTTP_STATUS: {
+    OK: 200,
+    BAD_REQUEST: 400,
+    UNPROCESSABLE_REQ: 422,
+    INTERNAL_ERROR: 500,
+  },
+};
+
+module.exports = constants;
